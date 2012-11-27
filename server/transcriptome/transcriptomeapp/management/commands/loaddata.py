ce9cdefbf8bb695ee33566788b69414f93317449
Chidube Ezeozue <cezeozue@yahoo.com>
2012-11-27 01:22:30 +0100

    Added work done on server-side to mid-course report

--- server/transcriptome/transcriptomeapp/management/commands/loaddata.py
+++ server/transcriptome/transcriptomeapp/management/commands/loaddata.py
@@ -7,7 +7,9 @@ class Command(BaseCommand):
     help = 'Loads the database with reads'
     
     def handle(self, *args, **options):
-        print 'Script called' + args[0] + args[1]
+        line_count_step = 100
+        
+        line_count = 0
         gtf_file_name = args[0]
         input_file_name = args[1]
         expt_desc = args[2]
@@ -18,8 +20,12 @@ class Command(BaseCommand):
         experiment.save()
         
         exons = {}
+        print 'Reading gtf'
         for line in gtf_file:
-            print line
+            line_count += 1
+            if line_count % line_count_step == 0:
+                print 'Working on gtf line ' + str(line_count)            
+            
             tokens = line.split('\t')
             
             exon_start = int(tokens[3])
@@ -27,7 +33,6 @@ class Command(BaseCommand):
             
             details = tokens[8]
             details_tokens = details.split(';')
-            print details_tokens
             
             gene_id = details_tokens[0].strip()
             gene_id = gene_id[9:len(gene_id) - 1]
@@ -47,17 +52,18 @@ class Command(BaseCommand):
                 if existing_possible_match:
                     exon = possible_match[2]
                 else:
-                    exon = Exon.objects.get(start__lt=existing[1], end__gt=existing[0])
+                    try:
+                        gene = Gene.objects.get(name=gene_id)
+                    except Gene.DoesNotExist:
+                        gene = Gene(name=gene_id)
+                        gene.save()
+                        
+                    exon = Exon.objects.get(start__lt=existing[1], end__gt=existing[0], gene=gene)
+                                            
                 exon.start = existing[0]
                 exon.end = existing[1]
                 exon.save()                
-            except Exon.DoesNotExist:
-                try:
-                    gene = Gene.objects.get(name=gene_id)
-                except Gene.DoesNotExist:
-                    gene = Gene(name=gene_id)
-                    gene.save()
-                    
+            except Exon.DoesNotExist:                    
                 exon = Exon(start=existing[0], end=existing[1], gene=gene)
                 exon.save()
             
@@ -69,7 +75,13 @@ class Command(BaseCommand):
                     
         input_file = open(input_file_name)
         
+        print 'Reading reads'
+        line_count = 0
         for line in input_file:
+            line_count += 1
+            if line_count % line_count_step == 0:
+                print 'Working on read line ' + str(line_count) 
+                
             tokens = line.split('\t')
             
             if tokens[0].rstrip() == 'polyA':
@@ -78,7 +90,6 @@ class Command(BaseCommand):
             read_start = int(tokens[1])
             read_stop = int(tokens[2])            
             exon_num = int(tokens[9])
-            print 'exon_num=' + str(exon_num)
             
             # Hack to make searching faster. May not work on TopHat output
             desc = tokens[3]
@@ -91,59 +102,27 @@ class Command(BaseCommand):
             
             possible_matches = exons[gene_id]
             for possible_match in possible_matches:
-                print str(possible_match) + " : " + str([read_start, read_stop]) + " : " + str((possible_match[1] <= read_start or possible_match[0] >= read_stop))
                 if not (possible_match[1] <= read_start or possible_match[0] >= read_stop):
                     matches.append(possible_match)
                     
                     if len(matches) >= exon_num:
-                        print 'found enough matching exons'
                         break;
-            
-            print 'matched exons=' + str(len(matches))        
-            
+                        
             matches_exons = []
             for match in matches:
                 matches_exons.append(match[2])
                 
             try:
-                try:
-#                    read = Read.objects.annotate(num_exons=Count('exons')).get(exons__in=matches_exons, num_exons=len(matches_exons), experiment=experiment)
-                    read = Read.objects.annotate(num_exons=Count('exons')).filter(num_exons=len(matches_exons), experiment=experiment)
-                    for t_matches_exon in matches_exons:
-                        read = read.filter(exons__id__contains=t_matches_exon.id)
-                    read = read.get()
-                except Read.MultipleObjectsReturned:
-                    print "Exception thrown"
-                    read = Read.objects.annotate(num_exons=Count('exons')).filter(exons__in=matches_exons, num_exons=len(matches_exons), experiment=experiment)
-                    reads = read.all()
-                    print "Num of reads found=" + str(len(reads))
-                    for read in reads:
-                        print [str(read_exon.start) + "-" + str(read_exon.end) for read_exon in read.exons.all()]
-                    sys.exit()
+                read = Read.objects.annotate(num_exons=Count('exons')).filter(num_exons=len(matches_exons), experiment=experiment)
+                for t_matches_exon in matches_exons:
+                    read = read.filter(exons__id__contains=t_matches_exon.id)
+                read = read.get()
                     
-                print "Number of exons in read" + str(read.exons.count())
                 read.readCount = read.readCount + 1
                 read.save()
-            except Read.DoesNotExist: 
-                print "Could not find read" 
-                print [t_matches_exon.id for t_matches_exon in matches_exons]
-                tmp = Read.objects.annotate(num_exons=Count('exons')).filter(num_exons=len(matches_exons), experiment=experiment)
-                for t_matches_exon in matches_exons:
-                    tmp = tmp.filter(exons__id__contains=t_matches_exon.id)
-                tmp = tmp.all()
-                
-                for t in tmp:
-                    print t.num_exons
-                    
-                tmp = Read.objects.annotate(num_exons=Count('exons')).filter(num_exons=len(matches_exons), experiment=experiment)
-                tmp = tmp.all()
-                if tmp:
-                    for t_exon in tmp[0].exons.all():
-                        print 'Exon id: ' + str(t_exon.id)
-                
+            except Read.DoesNotExist:                 
                 read = Read(experiment=experiment, readCount=1)
                 read.save()
                 for matches_exon in matches_exons:
-                    print "Adding read: " + str(read_start) + "-" + str(read_stop) + " : " + str(matches_exon.start) + " : " + str(matches_exon.end) + " : " + str(matches_exon.id) 
                     read.exons.add(matches_exon) 
             
