3539d240a0022738d79eb0b0e8b25c940e5ba6f8
Chidube Ezeozue <cezeozue@yahoo.com>
2012-12-02 22:33:53 +0100

    First pass at pulling read data from server

--- server/transcriptome/transcriptomeapp/urls.py
+++ server/transcriptome/transcriptomeapp/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'transcriptome.transcriptomeapp.views.home', name='home'),
+    url(r'^get_puzzle', 'transcriptome.transcriptomeapp.views.get_puzzle', name='get_puzzle')
 )
